c651b7857736231d47a361c676b0b9c597da9c36
Nat Wilson <njwilson23@gmail.com>
2015-12-14 11:51:58 -0800

    mask points within DEM extents

--- src/MELTPACK/dhdt.py
+++ src/MELTPACK/dhdt.py
@@ -3,20 +3,20 @@
 import karta
 import numpy as np
 
-def lagrangian_dhdt(g1, g2, u, v, timespan):
+def lagrangian_dhdt(g1, g2, uvel, vvel, timespan=datetime.timedelta(days=1)):
     """ Compute the Lagrangian time derivative of change between two grids.
 
     Arguments:
     ----------
     g1: RegularGrid
     g2: RegularGrid
-    u: RegularGrid, horizontal velocity component
-    v: RegularGrid, vertical velocity component
-    timespan: datetime.timedelta, temporal baseline
+    uvel: RegularGrid, horizontal velocity component
+    vvel: RegularGrid, vertical velocity component
+    timespan: datetime.timedelta [optional], temporal baseline, default 1 day
 
     Returns:
     --------
-    ndarray
+    ndarray with positions, displacements, and lagrangian elevation change
 
         [x_0, x_1 .....
          y_0, y_1 .....
@@ -41,12 +41,25 @@ def lagrangian_dhdt(g1, g2, u, v, timespan):
     dx = u*timespan.days/365.0 
     dy = v*timespan.days/365.0 
 
+    # mask out points that aren't within both grids
+    def within_bbox(x, y, bbox, margin):
+        return (x > bbox[0]+margin[0]) & (x < bbox[2]-margin[0]) & \
+               (y > bbox[1]+margin[1]) & (y < bbox[3]-margin[1])
+    
+    mask = within_bbox(x, y, g1.bbox, g1.transform[2:4]) & \
+           within_bbox(x+dx, y+dx, g2.bbox, g2.transform[2:4])
+
+    x = x[mask]
+    y = y[mask]
+    dx = dx[mask]
+    dy = dy[mask]
+
     # sample g1 at original locations
     z1 = g1.sample(x, y)
     # sample g2 at displaced locations
     z2 = g2.sample(x+dx, y+dy)
 
     # limit results to where data exists in both grids
-    m = np.isnan(z1) | np.isnan(z2)
-    return np.vstack([x[~m], y[~m], dx[~m], dy[~m], z1[~m], z2[~m]])
+    m = ~np.isnan(z1) | ~np.isnan(z2)
+    return np.vstack([x[m], y[m], dx[m], dy[m], z1[m], z2[m]])
 
