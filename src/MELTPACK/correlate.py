04339e427bad65538eb30a5ede6a1400286d1c0a
Nat Wilson <njwilson23@gmail.com>
2015-12-30 16:05:44 -0800

    correlation improvements
    - check for zero std deviation
    - use mode="valid" for autocorrelation by default
    - clip scenes to a common area (a bit buggy)
    - fix chunk overlap issue leading to obvious tile artefacts

--- src/MELTPACK/correlate.py
+++ src/MELTPACK/correlate.py
@@ -4,11 +4,17 @@ from multiprocessing import cpu_count
 import numpy as np
 from scipy import signal
 
-def _normalize_chip(chip):
-    return (chip-chip.mean())/chip.std()
+from . import utilities
 
-def _autocorrelate(search_chip, ref_chip):
-    return signal.fftconvolve(search_chip, ref_chip[::-1,::-1], mode="same")
+def _normalize_chip(chip):
+    s = chip.std()
+    if s == 0.0:
+        return np.zeros_like(chip)
+    else:
+        return (chip-chip.mean())/chip.std()
+
+def _autocorrelate(search_chip, ref_chip, mode="valid"):
+    return signal.fftconvolve(search_chip, ref_chip[::-1,::-1], mode=mode)
 
 def findpeak(c):
     """ Return in the integer row, column indices of the largest value in array
@@ -26,12 +32,15 @@ def findoffset(size, peak):
     x = peak[1] - size[1]/2
     return x, y
 
-def correlate_chips(search_chip, ref_chip):
+def correlate_chips(search_chip, ref_chip, mode="valid"):
     """ Given two images, compute offsets using normalized cross-correlation.
     This is intended to be useful for image co-registration for feature
     tracking.
+
+    Set `mode="same"` for image co-registration. Use `mode="valid"` for feature
+    tracking.
     """
-    c = _autocorrelate(_normalize_chip(search_chip), _normalize_chip(ref_chip))
+    c = _autocorrelate(_normalize_chip(search_chip), _normalize_chip(ref_chip), mode=mode)
     i, j = findpeak(c)
     return findoffset(search_chip.shape, (i, j)), c[i,j]
 
@@ -49,22 +58,25 @@ def _do_correlation(search_img, ref_img, center_coords):
     y_displ = ref_center[1] - yg
     return ref_center, (x_displ, y_displ), strength
 
-def correlate_scenes(scene1, scene2,
-        search_chip_size=(256, 256),
-        ref_chip_size=(32, 32),
+def correlate_scenes(scene1, scene2, search_size=(256, 256), ref_size=(32, 32),
         resolution=(16, 16)):
 
-    dx, dy = scene2.transform[2:4]
+    bboxc = utilities.overlap_bbox(scene1.data_bbox, scene2.data_bbox)
+    scene1c = scene1.clip(bboxc[0], bboxc[2], bboxc[1], bboxc[3])
+    scene2c = scene2.clip(bboxc[0], bboxc[2], bboxc[1], bboxc[3])
+    # scene1c = scene1
+    # scene2c = scene2
+    dx, dy = scene2c.transform[2:4]
 
     points = []
     displs = []
     strengths = []
 
     nprocs = cpu_count()
-    overlap = (ref_chip_size[0]-resolution[0], ref_chip_size[1]-resolution[1])
+    overlap = (ref_size[0]-resolution[0], ref_size[1]-resolution[1])
 
-    for chunk1, chunk2 in zip(scene1.aschunks(search_chip_size),
-                              scene2.aschunks(search_chip_size)):
+    for chunk1, chunk2 in zip(scene1c.aschunks(search_size, ref_size),
+                              scene2c.aschunks(search_size, ref_size)):
 
         bbox1 = chunk1.bbox
         bbox2 = chunk2.bbox
@@ -73,7 +85,7 @@ def correlate_scenes(scene1, scene2,
 
         futures = []
         with ThreadPoolExecutor(nprocs) as executor:
-            for ref_chip in chunk2.aschunks(ref_chip_size, overlap):
+            for ref_chip in chunk2.aschunks(ref_size, overlap):
                 fut = executor.submit(_do_correlation, chunk1, ref_chip,
                                       search_center)
                 futures.append(fut)
