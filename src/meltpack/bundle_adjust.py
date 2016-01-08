b0b7791ae23020ae82c84a8c2d5297aae00399d8
Nat Wilson <njwilson23@gmail.com>
2016-01-08 11:22:19 -0800

    bundle adjust fixes
    - ensure that the sume of the corrections is zero by adding an
      additional constraint
    - use pseudoinverse to solve least squares problem. When all data are
      used, the matrix product of the combination matrix is a singular
      Toeplitx matrix that looks like
    
      [4 -1 -1 -1 -1]
      [-1 4 -1 -1 -1]
      [-1 -1 4 -1 -1]
      [-1 -1 -1 4 -1]
      [-1 -1 -1 -1 4]
    
      A good solution can still be obtained using the SVD (shortcut through
      numpy's linalg.pinv function).

--- src/meltpack/bundle_adjust.py
+++ src/meltpack/bundle_adjust.py
@@ -49,9 +49,92 @@ def compute_vertical_corrections(grid_fnms, min_pixel_overlap=100,
 
     c, C = _comparison_matrix(len(grid_fnms))
 
-    # Piecewise comparison
+    # Piecewise comparison (computes the matrix-vector product CD)
     CD = []
     #S = []
+    for i, j in c:
+        dem0 = karta.read_gtiff(grid_fnms[i])
+        dem1 = karta.read_gtiff(grid_fnms[j])
+        dem0.values[dem0.values<-1000000] = np.nan
+        dem1.values[dem1.values<-1000000] = np.nan
+
+        # If bedrock regions are provided, limit the overlapping pixel counts
+        # the regions inside the bedrock masking polygons.
+        # Iterate through all polygons, keeping track of pixels that are in at
+        # least one polygon in the *mask_count* array variables.
+        # Then, set all DEM pixel where *mask_count* is zero to NODATA
+        if polymasks is not None and len(polymasks)!=0:
+            mask_count0 = np.zeros(dem0.size, dtype=np.int16)
+            mask_count1 = np.zeros(dem1.size, dtype=np.int16)
+            for poly in polymasks:
+
+                x = [a[0] for a in poly.get_vertices(dem0.crs)]
+                y = [a[1] for a in poly.get_vertices(dem0.crs)]
+
+                ny, nx = dem0.size
+                msk0 = karta.raster.grid.mask_poly(x, y, nx, ny, dem0.transform)
+                ny, nx = dem1.size
+                msk1 = karta.raster.grid.mask_poly(x, y, nx, ny, dem1.transform)
+
+                mask_count0[msk0] += 1
+                mask_count1[msk1] += 1
+
+            dem0.values[mask_count0==0] = dem0.nodata
+            dem1.values[mask_count1==0] = dem1.nodata
+
+        n = utilities.count_shared_pixels(dem0, dem1)
+        if n >= min_pixel_overlap:
+            CD.append(np.mean(utilities.difference_shared_pixels(dem0, dem1)))
+            #S.append(np.nanstd(dem0.values-dem1.values))
+        else:
+            CD.append(np.nan)
+            #S.append(np.nan)
+        del dem0, dem1
+
+    # Augment C by removing rows where there is no appreciable overlap
+    Ca = C[~np.isnan(CD),:]             # Drop non-ovelapping relations
+    CaD = np.array(CD)[~np.isnan(CD)]
+
+    corrected_grids = [fnm for tf, fnm in zip(~np.all(Ca==0, axis=0), grid_fnms) if tf]
+    Ca = Ca[:,~np.all(Ca==0, axis=0)]   # Drop DEMs not involved in remaining relations
+
+    # Compute weights
+    Wdiag = np.zeros(len(Ca))
+    for i,row in enumerate(Ca):
+        fnm1 = corrected_grids[np.argmin(row)]
+        fnm2 = corrected_grids[np.argmax(row)]
+        Wdiag[i] = weighting_func(fnm1, fnm2)
+
+    W = np.diag(Wdiag)
+    Winv = np.linalg.inv(W)
+
+    # Solve the weighted least-squares problem: C^T W^-1 C dz = -C^T W^-1 C D
+    #dz = np.linalg.solve(np.dot(np.dot(Ca.T, Winv), Ca),
+    #                     np.dot(np.dot(Ca.T, Winv), -CaD))
+
+    A = np.dot(np.dot(Ca.T, Winv), Ca)
+    RHS = np.dot(np.dot(Ca.T, Winv), -CaD)
+
+    # Append a constraint to ensure that the mean of dz is zero
+    n = A.shape[0]
+    A = np.r_[np.c_[A, np.zeros(n)], np.ones([1,n+1])]
+    RHS = np.r_[RHS, 0.0]
+
+    # A^TA may be singular, so solve system with the pseudoinverse
+    dz = np.dot(np.linalg.pinv(A), RHS)[:-1]
+    return {fnm: _dz for fnm, _dz in zip(corrected_grids, dz)}
+
+def compute_horizontal_corrections(grid_fnms, min_pixel_overlap=100,
+    weighting_func=None, polymasks=()):
+    """ Compute the least squares horizontal (dx, dy) corrections to align two
+    images that share at least *min_pixel_overlap* pixels. """
+    if weighting_func is None:
+        weighting_func = lambda a,b: 1.0
+
+    c, C = _comparison_matrix(len(grid_fnms))
+
+    # Piecewise comparison
+    CD = []
     for i, j in c:
         dem0 = karta.read_gtiff(grid_fnms[i])
         dem1 = karta.read_gtiff(grid_fnms[j])
@@ -85,10 +168,8 @@ def compute_vertical_corrections(grid_fnms, min_pixel_overlap=100,
         n = utilities.count_shared_pixels(dem0, dem1)
         if n >= min_pixel_overlap:
             CD.append(np.mean(utilities.difference_shared_pixels(dem0, dem1)))
-            #S.append(np.nanstd(dem0.values-dem1.values))
         else:
             CD.append(np.nan)
-            #S.append(np.nan)
         del dem0, dem1
 
     # Augment C by removing rows where there is no appreciable overlap
@@ -118,3 +199,6 @@ def compute_vertical_corrections(grid_fnms, min_pixel_overlap=100,
     dz = np.linalg.solve(np.dot(np.dot(Ca.T, Winv), Ca),
                          np.dot(np.dot(Ca.T, Winv), -CaD))
     return {fnm: _dz for fnm, _dz in zip(corrected_grids, dz)}
+    
+    raise NotImplementedError()
+
