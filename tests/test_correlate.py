238b75db43cbef6955bf1cbb3e6ff6670e929653
Nat Wilson <njwilson23@gmail.com>
2016-01-22 10:03:58 -0800

    update manual test

--- tests/test_correlate.py
+++ tests/test_correlate.py
@@ -1,15 +1,20 @@
 import MELTPACK.correlate as mpc
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches
 
-x, y = np.meshgrid(np.linspace(0, 1), np.linspace(0, 1))
+x, y = np.meshgrid(np.linspace(0, 1, 128), np.linspace(0, 1, 128))
 
 im0 = (30*(0.25-(x-0.5)**2) + 20*(0.25-(y-0.5)**2)).astype(np.float32)
+im0[40:85,60:66] = 11.0
 
-im1 = np.ascontiguousarray(im0[5:35, 7:37])
-im2 = np.ascontiguousarray(im0[10:40, 10:40])
+xoff = 44
+yoff = 78
+nx = 32
+ny = 32
 
-print(im1[0,0], im1[0,1])
+ims = np.ascontiguousarray(im0[:,:])
+imr = np.ascontiguousarray(im0[yoff:yoff+ny, xoff:xoff+nx])
 
 # subimage is called with the signature
 #   subimage(x0, y0,
@@ -17,11 +22,21 @@ print(im1[0,0], im1[0,1])
 #            in_buffer, in_width, in_height)
 
 plt.subplot(1, 2, 1)
-plt.imshow(im1, vmin=6, vmax=12)
+plt.imshow(ims, vmin=3, vmax=12, origin="bottom")
+rect = matplotlib.patches.Rectangle((xoff-1, yoff-1), nx, ny, edgecolor="k", facecolor="none")
+plt.gca().add_patch(rect)
+plt.title("search image")
 plt.subplot(1, 2, 2)
-plt.imshow(im2, vmin=6, vmax=12)
+plt.imshow(imr, vmin=3, vmax=12, origin="bottom")
+plt.title("reference image")
 
-res = mpc.correlate(im1, im2, min_corr_strength=1.0, max_dist=10.0)
-print(res)
+res = mpc.correlate(ims, imr, min_corr_strength=2.0, max_dist=80.0,
+                    max_srch_offset=np.array([64.0, 64.0], np.float32),
+                    fit_method=1)
+print("Exit code:     %d" % res[0])
+print("Corr strength: %.5f" % res[1])
+print("Best fit:      %.3f, %.3f" % (res[2], res[3]))
+print("Error est.:    %.3f, %.3f" % (res[4], res[5]))
+print("Diag. displ.:  %.3f" % res[6])
 
 plt.show()
