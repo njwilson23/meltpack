3455737eaf4116f1e2c4f85f3115e9f3da7ef2ca
Nat Wilson <njwilson23@gmail.com>
2016-01-22 10:11:11 -0800

    remove build instructions for Imcorr

--- setup.py
+++ setup.py
@@ -2,19 +2,10 @@ from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 import numpy
 
-CORR_OBJECTS = ["cross.o", "eval.o", "fft2d.o", "fitreg.o", "gcorr.o",
-                "gnorm.o", "kvert.o", "sums.o", "esterr.o"]
-
-extensions = [Extension("meltpack.c_correlate", ["src/meltpack/c_correlate.pyx"],
-                         extra_objects=CORR_OBJECTS,
-                         include_dirs=[numpy.get_include(),
-                                       "/usr/lib/gcc/x86_64-linux-gnu/5"],
-                         libraries=["gfortran"]),
-               Extension("meltpack._divergence", ["src/meltpack/_divergence.pyx"],
-                          include_dirs=[numpy.get_include()]),
-               Extension("meltpack._smooth", ["src/meltpack/_smooth.pyx"],
-                          include_dirs=[numpy.get_include()]),
-]
+extensions = [Extension("meltpack._divergence", ["src/meltpack/_divergence.pyx"],
+                        include_dirs=[numpy.get_include()]),
+              Extension("meltpack._smooth", ["src/meltpack/_smooth.pyx"],
+                        include_dirs=[numpy.get_include()])]
 
 setup(
         name="meltpack",
